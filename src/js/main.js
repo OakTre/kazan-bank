eb9069c81a172f04fa2a61330b23a2d040a44342
Nail <nailgalimov8@gmail.com>
2021-11-23 17:04:35 +0300

    page-succes, page-fail

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import documentReady from "./helpers/documentReady";
 import lazyImages from "./modules/lazyImages";
+import phoneMask from './modules/phoneMask';
+import validation from './modules/validation';
 
 documentReady(() => {
 	lazyImages();
+	phoneMask();
+	validation();
 });
